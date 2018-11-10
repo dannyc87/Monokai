d5ef39ad7dfd13adf061af9ef54bb8578df676ee
Dani <dani@localhost.localdomain>
2018-11-10 17:24:09 +0100

    initial work on monokai color palette

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,6 @@ gulp.task('styles', function(done) {
     gulp.src('gtk-3.0/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gtk-3.0/'))
-        .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Nordic"'))
     done();
 });
 
@@ -14,7 +13,6 @@ gulp.task('shell-style', function(done) {
     gulp.src('gnome-shell/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gnome-shell/'))
-        .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Nordic"'))
     done();
 });
 
