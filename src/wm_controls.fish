da207acedecea8f9eb395ee1ee3ab11e877f6c31
Dani <dani@localhost.localdomain>
2018-11-18 16:46:24 +0100

    rendered wm graphics, added gtk2 src folder

--- src/wm_controls.fish
+++ src/wm_controls.fish
@@ -1,5 +1,8 @@
+!/usr/local/bin/fish
+
 set -l  wm 'close' 'close_prelight' 'close_unfocused' 'min' 'min_prelight' 'maximize' 'maximize_prelight'
 set -l path '../assets/'
 for item in $wm
-	inkscape -z $path$item.svg -e $path$item@2.png --export-dpi=192; optipng -o7 --quiet $path$item@2.png 
+	inkscape -z $path$item.svg -e $path$item@2.png --export-dpi=192; optipng -o7 --quiet $path$item@2.png;
+	inkscape -z $path$item.svg -e $path$item.png --export-dpi=96; optipng -o7 --quiet $path$item.png;
 end
